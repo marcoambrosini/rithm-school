92598f0157ab3f5e8c6c9300c19bb8b8d1b41020
mrc <marcoambrosini@protonmail.com>
2018-12-14 15:02:31 +0100

    car game

--- 02 Intermediate Javascript I/5 events and local storage/script.js
+++ 02 Intermediate Javascript I/5 events and local storage/script.js
@@ -1,6 +1,8 @@
-//task1
+/* jshint esversion: 6 */
 
-window.onload = function() {
+//PART 1 task1
+
+window.onload = function () {
 
 	//task2
 	document.getElementById('change_heading').innerHTML = 'Hello, World!';
@@ -8,9 +10,9 @@ window.onload = function() {
 
 	//task3 When a user hovers over one of the colored boxes change the text to display the color that is being hovered over.
 
-	var section = document.querySelector ('section');
-	section.addEventListener('mouseover', function(event) {
-		var selectedColor  = document.querySelector('.selected');
+	var section = document.querySelector('section');
+	section.addEventListener('mouseover', function (event) {
+		var selectedColor = document.querySelector('.selected');
 		selectedColor.innerText = event.target.className;
 	});
 
@@ -26,4 +28,37 @@ window.onload = function() {
 	//Append your new div to the page to the section tag.
 
 	section.appendChild(newDiv);
+
+
+	//PART 2 Create a racing game with the two cars. When the race button is pressed, the two cars should move across the screen until one of them is at the end of the screen. When one of the blocks reaches the end - you should alert "winner!"
+
+	//select the button and the cars
+	var button = document.querySelector('button');
+	var car1 = document.querySelector('.car1');
+	var car2 = document.querySelector('.car2');
+	//create 2 margin counters
+	let margin1 = 0;
+	let margin2 = 0;
+
+
+	//add listener
+	button.addEventListener("click", function (event1) {
+		//set timer 
+		let timerId = setInterval(function () {
+			//move the 2 cars at different increments
+			car1.style.marginLeft = margin1 + 'px';
+			car2.style.marginLeft = margin2 + 'px';
+			margin1 += 10;
+			margin2 += 8;
+			if (margin1 >= window.innerWidth - 50) {
+				clearInterval(timerId);
+				alert('1 wins the race!');
+			}
+		}, 100);
+
+		
+	});
+	
+		
+	
 };
